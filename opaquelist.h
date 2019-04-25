72ad0378af623cd211390ad7cca0ebf702161463
Adrien <grosjeanadrien@gmail.com>
2019-04-25 18:20:40 +0200

    Reference wrappers instead of pointers + at(id) instead of find(id)

--- opaquelist.h
+++ opaquelist.h
@@ -7,6 +7,7 @@
 #include <iterator>
 #include <stdexcept>
 #include <vector>
+#include <functional>
 
 namespace Oggy
 {
@@ -37,7 +38,7 @@ public:
     {
         ID id(m_nextID);
         m_valist.push_back(val);
-        m_umap.emplace(std::make_pair(id, &m_valist.back()));
+        m_umap.emplace(std::make_pair(id, std::ref(m_valist.back())));
         m_ids.push_back(id);
         incrementNextID();
         return id;
@@ -51,7 +52,7 @@ public:
      */
     VAL removeVal(ID id)
     {
-        const VAL* valptr = m_umap.find(id)->second;
+        const VAL* valptr = &(m_umap.at(id).get());
         for (auto it(m_ids.begin()); it != m_ids.end(); ++it){
             if (*it == id){
                 m_ids.erase(it);
@@ -67,14 +68,13 @@ public:
         throw std::invalid_argument("given ID to remove does not exist !");
     }
     
-    using miterator = typename std::unordered_map<ID, VAL* const>::iterator;
     using mconst_iterator = typename std::unordered_map<ID, VAL* const>::const_iterator;
 
     using viterator = typename std::list<VAL>::iterator;
     using vconst_iterator = typename std::list<VAL>::const_iterator;
 
-    inline miterator find (const ID& id) { return m_umap.find(id); }
-    inline mconst_iterator find (const ID& id) const { return m_umap.find(id); }
+    inline VAL& at (const ID& id) { return m_umap.at(id).get(); }
+    inline const VAL& at (const ID& id) const { return m_umap.at(id).get(); }
     
     inline unsigned int count (const ID& id) const { return m_umap.count(id); }
 
@@ -104,7 +104,7 @@ public:
 
 private:
     ID m_nextID;
-    std::unordered_map<ID, VAL* const> m_umap;
+    std::unordered_map<ID, std::reference_wrapper<VAL> const> m_umap;
     std::list<VAL> m_valist;
     std::vector<ID> m_ids;
 };
